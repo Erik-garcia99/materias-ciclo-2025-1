222b49378071c3059f3bd3835e3d139316dac2d6
dmrx <dmrx@gmail.com>
2025-04-28 23:11:29 -0700

    resulto lo del boton, ya de aqui pal real ready

--- ucontroladores/laboratorio/lab5/practica5_CodeBlocsk/main.c
+++ ucontroladores/laboratorio/lab5/practica5_CodeBlocsk/main.c
@@ -5,12 +5,12 @@
 #define ClrBitPort(port, bit) __asm__ __volatile__("cbi %0, %1" : : "I"(_SFR_IO_ADDR(port)), "I"(bit) : "memory")
 
 #define TIME_WINDOW 300
-#define SEED xx
+#define SEED 0x01
 /*******************/
 #define BTN_PIN PF7
-#define NOT_PRESSED 0
-#define SHORT_PRESSED 1
-#define LONG_PRESSED 2
+//#define NOT_PRESSED 0
+//#define SHORT_PRESSED 1
+//#define LONG_PRESSED 2
 
 enum ButtonStates
 {
@@ -88,17 +88,20 @@ void InitPorts(void); // al parecer este va asre igual porque en cada led se va 
 
 uint8_t check_Btn(void); // OK
 
-void updateLeds(uint8_t gameState, uint8_t lectura);
+void updateLeds(uint8_t gameState);
 
 /*
 mis prototipos
 
 */
 
-void waitState(uint8_t lectura_PIN);
+void waitState();
+void startGame();
 
 // Global variable
 uint32_t millis;
+uint32_t lastUpdate=0;
+volatile uint8_t _start_game_=0;
 
 int main(void)
 {
@@ -110,6 +113,7 @@ int main(void)
     uint8_t bucle = 1;
     // InitPorts();
     uint8_t lectura;
+    uint8_t btn;
 
     // configurar los pines 0 - 3  como salida
 
@@ -119,6 +123,9 @@ int main(void)
 
     while (1)
     {
+
+        //btn=  check_Bt();
+
         switch (check_Btn())
         {
         case eBtnShortPressed:
@@ -139,22 +146,32 @@ int main(void)
             break;
         }
         case eWaitForStart:
+            //el juego se va a inicar cunado se haga un longPress, quiere decir que al principio estara apagado no es asi
+            /*
+                esto es lo que va a pasar cunando se inice el juego,
+                para inicar el juego debemos de presionar el boton un largo rato, este nos dara nuestro numero random
+                que sera el tiempo que este nos daera para ganarle al juego, entre que apaga los leds y tenemos
+                un tiempo limite para presionar el boton
+
+                entonces como podemos hacer para pasar de los leds de wait al otro estado?
+
+            */
+
             // cuando se presione la priemra vez lo que hara es que ser mirara ese patron de espera que hemos diseniado
             // por lo que debemos se saber, el como implementar
 
             // ya se como va esta madre, este va a esperar mientras no se precione el boton este tendra que mostrar la secuencia
             // de espera, cuendo se presione este se va a detener y <currentGameState> va a incrementar en 1
             // que dara inicio al juego
-            lectura = check_Btn();
+            //lectura = check_Btn();
 
-            updateLeds(eWaitForStart, lectura);
+            //updateLeds(eWaitForStart);
 
-            if (lectura == SHORT_PRESSED)
-            {
-                bucle = 0;
-                currentGameState++; // cunado se cumple pasamos al sigueinte caso, al inicio del juego
-                PORTF &= ~0xBF;
-            }
+            /*if(btn == eBtnShortPressed){
+                PORTF &= 0xF0;
+                _start_game_=1;
+                currentGameState = eStartCount;
+            }*/
 
             break;
         case eStartCount:
@@ -162,6 +179,7 @@ int main(void)
             countdown--;
             if (countdown == 0)
                 currentGameState++;
+
             break;
         }
         case eEndCount:
@@ -182,7 +200,7 @@ int main(void)
         }
         }
 
-        updateLeds(currentGameState, uint8_t lectura);
+        updateLeds(currentGameState);
         delay(1);
         millis++;
     }
@@ -224,7 +242,7 @@ uint8_t check_Btn(void)
     if (PINF & (1 << BTN_PIN))
     {
 
-        return NOT_PRESSED;
+        return eBtnUndefined;
     }
 
     uint16_t tiempo_presionado = 0;
@@ -251,7 +269,7 @@ uint8_t check_Btn(void)
                 delay(1);
             }
             delay(20);
-            return LONG_PRESSED;
+            return  eBtnLongPressed;
         }
     }
 
@@ -259,50 +277,68 @@ uint8_t check_Btn(void)
     if (PINF & (1 << BTN_PIN))
     {
 
-        return SHORT_PRESSED;
+        return eBtnShortPressed;
     }
-
-    return 0;
 }
 
-void updateLeds(uint8_t gameState, uint8_t lectura_PIN)
+void updateLeds(uint8_t gameState)
 {
     // esta funcion debe de devolver estos estados en los cuales se esta haceindo los cambios necesarios para los leds
 
     // por lo que creo que deberia de ser un switch o esta funcion invocar a otras que haga ese proceso.
 
+
     switch (gameState)
     {
 
-    case eWaitForStart:
-    {
-        waitState(lectura_PIN);
-    }
+        case eWaitForStart:
+        {
+            waitState();
+            break;
+        }
+
+        case eStartCount:
+            startGame();
+            break;
+
+
+
     }
 }
 
-void waitState(uint8_t lectura_PIN)
-{
+/*
+void waitState(uint8_t btn) {
+    static uint32_t lastChange = 0;
+    uint8_t indice=0;
 
-    // esta funcion lo unico que me va a hacer es hacer esa secuencia
-    // tan solo hace la secuncia de espera
-
-    // al parecer, este bucle nunca sale por lo que la salida debe de ser desde aqui
-    volatile uint8_t indice = 0;
-
-    while (1)
-    {
-
-        while (indice < 8 && lectura_PIN == NOT_PRESSED)
-        {
-            //
-
+    while(1){
+        while(indice<8){
             DDRF = _CONF_DDR[indice];
             PORTF = _CONF_PORT[indice];
             delay(100);
             indice++;
         }
-
-        indice = 0;
+        indice =0;
+        if(_start_game_==1){
+            break;
+        }
     }
+}*/
+
+void waitState() {
+
+    volatile uint8_t indice=0;
+    while(indice<8){
+        DDRF = _CONF_DDR[indice];
+        PORTF =_CONF_PORT[indice];
+        indice++;
+    }
+}
+
+
+void startGame() {
+
+    uint8_t indice  = (myRand(SEED) + 1) * 20;
+
+
 }
