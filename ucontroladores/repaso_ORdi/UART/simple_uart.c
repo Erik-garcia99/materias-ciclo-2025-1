04e159756afedc86001c8b73a0057e98f2676271
erik garcia <73853913+Erik-garcia99@users.noreply.github.com>
2025-05-25 18:17:02 -0700

    repaso UART

---
+++ ucontroladores/repaso_ORdi/UART/simple_uart.c
@@ -0,0 +1,12 @@
+#include<avr/io.h>
+
+
+
+void uart_init(void){
+
+
+	//para clauclar el baudaje debemos mandarle el mismo que queremos pero se ingresa a UBRR que es el prescalador 
+	UBRR = 
+
+
+}
